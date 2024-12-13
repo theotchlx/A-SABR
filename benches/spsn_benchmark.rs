7992dd0d16648b8945c919cbb65239a47de1c935
Nicolas Le Borgne <nicolb.dev@gmail.com>
2024-12-13 18:33:12 +0100

    Demonstrate benchmark comparison between SpsnMpt SABR vs Hop

--- benches/spsn_benchmark.rs
+++ benches/spsn_benchmark.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use std::{cell::RefCell, rc::Rc, time::Duration};
 
 use a_sabr::{
@@ -8,39 +7,51 @@ use a_sabr::{
 };
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+macro_rules! make_spsn_router {
+    ($router_type:ident, $ptvg_filepath:expr) => {{
+        let (nodes, contacts) =
+            TVGUtilContactPlan::parse::<SegmentationManager>($ptvg_filepath).unwrap();
+
+        let route_storage = Rc::new(RefCell::new(TreeCache::new(false, false, 10)));
+
+        $router_type::new(nodes, contacts, route_storage, false)
+    }};
+}
+
+macro_rules! blackbox_route_bundle {
+    ($router:ident, $source:expr, $bundle:expr, $curr_time:expr, $excluded_nodes:expr) => {
+        $router.route(
+            black_box($source),
+            black_box($bundle),
+            black_box($curr_time),
+            black_box($excluded_nodes),
+        )
+    };
+}
+
 pub fn spsn_mpt_benchmark(c: &mut Criterion) {
-    let contact_plan =
-        TVGUtilContactPlan::parse::<SegmentationManager>("benches/ptvg_files/sample1.json");
+    let ptvg_filepath = "benches/ptvg_files/sample1.json";
+    let mut router_sabr = make_spsn_router!(SpsnMpt, ptvg_filepath);
+    let mut router_hop = make_spsn_router!(SpsnHopMpt, ptvg_filepath);
 
-    match contact_plan {
-        Ok((nodes, contacts)) => {
-            let route_storage = Rc::new(RefCell::new(TreeCache::new(false, false, 10)));
-            let mut router = SpsnMpt::new(nodes, contacts, route_storage, false);
+    let source = 178;
+    let bundle = Bundle {
+        source: 178,
+        destinations: vec![159],
+        priority: 0,
+        size: 47419533.0,
+        expiration: 24060.0,
+    };
+    let curr_time = 60.0;
+    let excluded_nodes: Vec<NodeID> = vec![];
 
-            let source = 178;
-            let bundle = Bundle {
-                source: 178,
-                destinations: vec![159],
-                priority: 0,
-                size: 47419533.0,
-                expiration: 24060.0,
-            };
-            let curr_time = 60.0;
-            let excluded_nodes: Vec<NodeID> = vec![];
-
-            c.bench_function("route", |b| {
-                b.iter(|| {
-                    router.route(
-                        black_box(source),
-                        black_box(&bundle),
-                        black_box(curr_time),
-                        black_box(&excluded_nodes),
-                    )
-                })
-            });
-        }
-        Err(err) => panic!("Unable to load contact plan: {}", err),
-    }
+    let mut group = c.benchmark_group("SpsnMpt");
+    group.bench_function("SABR", |b| {
+        b.iter(|| blackbox_route_bundle!(router_sabr, source, &bundle, curr_time, &excluded_nodes))
+    });
+    group.bench_function("Hop", |b| {
+        b.iter(|| blackbox_route_bundle!(router_hop, source, &bundle, curr_time, &excluded_nodes))
+    });
 }
 
 criterion_group! {
