6d0d042afce673ab4a650fdaa6481970eb0bfa63
Olivier De Jonckere <olivier.de-jonckere@lirmm.fr>
2025-05-21 12:42:28 +0200

    Minimal main.rs

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::{cell::RefCell, env, rc::Rc};
+
+use a_sabr::{
+    bundle::Bundle,
+    contact_manager::{eto::ETOManager, evl::EVLManager, qd::QDManager, seg::SegmentationManager, ContactManager},
+    contact_plan::{asabr_file_lexer::FileLexer, from_asabr_lexer::ASABRContactPlan},
+    node_manager::none::NoManagement,
+    parsing::{coerce_cm, ContactDispatcher, Dispatcher},
+    route_storage::cache::TreeCache,
+    routing::{aliases::SpsnMpt, Router},
+};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <cp_file>", args[0]);
+        std::process::exit(1);
+    }
+    println!("Working with cp {}.", args[1]);
+
+    // We create a lexer to retrieve tokens from a file
+    let mut mylexer = FileLexer::new(&args[1]).unwrap();
+    let mut cp = ASABRContactPlan::new();
+
+    // All nodes will have the same management approach (NoManagement) but the contacts may be of various types
+    // We provide a map with markers that will allow the parser to create the correct contacts types thanks to
+    // the markers provides in the contact plan
+    let mut contact_dispatch: Dispatcher<ContactDispatcher> =
+        Dispatcher::<ContactDispatcher>::new();
+    contact_dispatch.add("evl", coerce_cm::<EVLManager>);
+    contact_dispatch.add("qd", coerce_cm::<QDManager>);
+    contact_dispatch.add("evl", coerce_cm::<ETOManager>);
+    contact_dispatch.add("seg", coerce_cm::<SegmentationManager>);
+
+
+    // We parse the contact plan (A-SABR format thanks to ASABRContactPlan) and the lexer
+    let (nodes, contacts) = cp
+        .parse::<NoManagement, Box<dyn ContactManager>>(&mut mylexer, None, Some(&contact_dispatch))
+        .unwrap();
+
+    // We create a storage for the Paths
+    let table = Rc::new(RefCell::new(TreeCache::new(true, false, 10)));
+    // We initialize the routing algorithm with the storage and the contacts/nodes created thanks to the parser
+    let mut spsn = SpsnMpt::<NoManagement, Box<dyn ContactManager>>::new(nodes, contacts, table, false);
+
+    // We will route a bundle
+    let b = Bundle {
+        source: 0,
+        destinations: vec![3, 4],
+        priority: 0,
+        size: 1.0,
+        expiration: 10000.0,
+    };
+
+    // We schedule the bundle (resource updates were conducted)
+    let out = spsn.route(0, &b, 0.0, &Vec::new());
+    // "out" provides the first hop contact / route (complete path) for the bundle
+}
