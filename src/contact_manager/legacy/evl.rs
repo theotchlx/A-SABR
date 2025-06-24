15ee74568de0b786f4ee9e3e1374bee2db7072a6
Olivier De Jonckere <olivier.de-jonckere@lirmm.fr>
2025-06-24 11:02:16 +0200

    Merge the two macros

--- src/contact_manager/legacy/evl.rs
+++ src/contact_manager/legacy/evl.rs
@@ -1,7 +1,7 @@
-use crate::{generate_basic_volume_manager, generate_prio_volume_manager};
+use crate::generate_prio_volume_manager;
 
 // With EVL, the delay due to the queue is not taken into account
 // and the updates are automatic (we do not "scan" an actual local queue),
 // we just reduce the volume available
-generate_basic_volume_manager!(EVLManager, false, true);
+generate_prio_volume_manager!(EVLManager, false, true, 1);
 generate_prio_volume_manager!(PEVLManager, false, true, 3);
--- src/contact_manager/legacy/mod.rs
+++ src/contact_manager/legacy/mod.rs
@@ -2,40 +2,10 @@ pub mod eto;
 pub mod evl;
 pub mod qd;
 
-/// A macro to conditionally implement `enqueue` and `dequeue` methods.
-///
-/// This macro takes a boolean value and implements the methods based on that value:
-/// - When `true`, no methods are implemented.
-/// - When `false`, the following methods are implemented:
-///   - `enqueue`: Adds a bundle to the queue.
-///   - `dequeue`: Removes a bundle from the queue.
 #[macro_export]
-macro_rules! impl_manual_queue_methods {
-    (true) => {
-        // No enqueue or dequeue methods when $auto_update is false
-    };
-
-    (false) => {
-        pub fn enqueue(&mut self, bundle: &crate::bundle::Bundle) {
-            let new_size = self.queue_size + bundle.size;
-            if new_size > self.original_volume {
-                panic!("Queue will overflow the contact's volume");
-            }
-            self.queue_size = new_size;
-        }
-
-        pub fn dequeue(&mut self, bundle: &crate::bundle::Bundle) {
-            if self.queue_size < bundle.size {
-                panic!("Attempting to dequeue a bundle larger than the current queue size");
-            }
-            self.queue_size -= bundle.size;
-        }
-    };
-}
-
-#[macro_export]
-macro_rules! generate_basic_volume_manager {
-    ($manager_name:ident, $add_delay:tt, $auto_update:tt)  => {
+macro_rules! generate_struct_management {
+    // if the priority count is set to 1, no priority, queue_size is not an array
+    ($manager_name:ident, 1) => {
         /// A simple manager for handling volcontact_data.startume and/or transmission delays (macro generated).
         #[cfg_attr(feature = "debug", derive(Debug))]
         pub struct $manager_name {
@@ -48,8 +18,7 @@ macro_rules! generate_basic_volume_manager {
             /// The total volume at initialization.
             original_volume: crate::types::Volume,
         }
-
-        impl $manager_name {
+         impl $manager_name {
             #[doc = concat!( "Creates a new `", stringify!($manager_name),"`  with specified average rate and delay.")]
             ///
             /// # Arguments
@@ -68,211 +37,29 @@ macro_rules! generate_basic_volume_manager {
                     original_volume: 0.0,
                 }
             }
-            crate::impl_manual_queue_methods!($auto_update);
-        }
-        impl crate::contact_manager::ContactManager for $manager_name {
-            /// Simulates the transmission of a bundle based on the contact data and available free intervals.
-            ///
-            #[doc = concat!( "The transmission time start time will be offset by the queue size: ", stringify!($add_delay),"`.")]
-            ///
-            /// # Arguments
-            ///
-            /// * `contact_data` - Reference to the contact information (unused in this implementation).
-            /// * `at_time` - The current time for scheduling purposes.
-            /// * `bundle` - The bundle to be transmitted.
-            ///
-            /// # Returns
-            ///
-            /// Optionally returns `ContactManagerTxData` with transmission start and end times, or `None` if the bundle can't be transmitted.
-            fn dry_run_tx(
-                &self,
-                contact_data: &crate::contact::ContactInfo,
-                at_time: crate::types::Date,
-                bundle: &crate::bundle::Bundle,
-            ) -> Option<crate::contact_manager::ContactManagerTxData> {
-                if bundle.size > self.original_volume - self.queue_size {
-                    return None;
-                }
-
-
-                let mut contact_start = contact_data.start;
-                // add_delay case 1 : if not eto, we push the eto from the contact start time
-                if ($auto_update && $add_delay) {
-                    contact_start += self.queue_size / self.rate;
-                }
-                let mut tx_start = if (contact_start > at_time) {
-                    contact_start
-                } else {
-                    at_time
-                };
-
-                // add_delay case 2 : eto, bundles are still in queue
-                if (!$auto_update && $add_delay) {
-                    tx_start += self.queue_size / self.rate;
-                }
-
-                let tx_end = tx_start + bundle.size / self.rate;
-                if tx_end > contact_data.end {
-                    return None;
-                }
-                Some(crate::contact_manager::ContactManagerTxData {
-                    tx_start,
-                    tx_end,
-                    delay: self.delay,
-                    expiration: contact_data.end,
-                    arrival: self.delay + tx_end,
-                })
+            #[inline(always)]
+            fn get_queue_size(&self, _bundle: &crate::bundle::Bundle) -> crate::types::Volume {
+                    self.queue_size
             }
-
-            /// Schedule the transmission of a bundle based on the contact data and available free intervals.
-            ///
-            /// This method shall be called after a dry run !Implementations might not ensure a clean behavior otherwise.
-            #[doc = concat!( "The queue volume will be updated by this method: ", stringify!($auto_update),"`.")]
-            ///contact_data.start
-            /// # Arguments
-            ///
-            /// * `contact_data` - Reference to the contact information (unused in this implementation).
-            /// * `at_time` - The current time for scheduling purposes.
-            /// * `bundle` - The bundle to be transmitted.
-            ///
-            /// # Returns
-            ///
-            /// Optionally returns `ContactManagerTxData` with transmission start and end times, or `None` if the bundle can't be transmitted.
-            fn schedule_tx(
-                &mut self,
-                contact_data: &crate::contact_manager::ContactInfo,
-                at_time: crate::types::Date,
-                bundle: &crate::bundle::Bundle,
-            ) -> Option<crate::contact_manager::ContactManagerTxData> {
-                if let Some(data) = self.dry_run_tx(contact_data, at_time, bundle) {
-                    // Conditionally update queue size based on $auto_update
-                    // Can overflow with overbooking
-                    if $auto_update {
-                        self.queue_size += bundle.size;
-                    }
-                    return Some(data);
-                }
-                None
+           #[inline(always)]
+            fn enqueue(&mut self, bundle: &crate::bundle::Bundle)  {
+                 self.queue_size += bundle.size;
             }
-
-            /// Initializes the segmentation manager by checking that rate and delay intervals have no gaps.
-            ///
-            /// # Arguments
-            ///
-            /// * `contact_data` - Reference to the contact information.
-            ///
-            /// # Returns
-            ///
-            /// Returns `true` if initialization is successful, or `false` if there are gaps in the intervals.
-            fn try_init(&mut self, contact_data: &crate::contact::ContactInfo) -> bool {
-                self.original_volume = (contact_data.end - contact_data.start) * self.rate;
-                true
-            }
-
-            /// Returns the original volume of the object.
-            ///
-            /// # Returns
-            ///
-            /// A `Volume` representing the original volume.
-            #[cfg(feature = "first_depleted")]
-            fn get_original_volume(&self) -> crate::types::Volume {
-                self.original_volume
-            }
-        }
-
-        /// Implements the DispatchParser to allow dynamic parsing.
-        impl crate::parsing::DispatchParser<$manager_name> for $manager_name {}
-
-        #[doc = concat!("Implements the `Parser` trait for ", stringify!($manager_name),"`, allowing the manager to be parsed from a lexer.")]
-        /// Implements the `Parser` trait for `SegmentationManager`, allowing the manager to be parsed from a lexer.
-        impl crate::parsing::Parser<$manager_name> for $manager_name {
-            #[doc = concat!("Parses a `", stringify!($manager_name),"` from the lexer, extracting the rate and delay intervals.")]
-            ///
-            /// # Arguments
-            ///
-            /// * `lexer` - The lexer used for parsing tokens.
-            /// * `_sub` - An optional map for handling custom parsing logic (unused here).
-            ///
-            /// # Returns
-            ///
-            /// Returns a `ParsingState` indicating whether parsing was successful (`Finished`) or encountered an error (`Error`).
-            fn parse(
-                lexer: &mut dyn crate::parsing::Lexer,
-            ) -> crate::parsing::ParsingState<$manager_name> {
-                let delay: crate::types::Duration;
-                let rate: crate::types::DataRate;
-
-                let rate_state = <crate::types::DataRate as crate::types::Token<crate::types::DataRate>>::parse(lexer);
-                match rate_state {
-                    crate::parsing::ParsingState::Finished(value) => rate = value,
-                    crate::parsing::ParsingState::Error(msg) => return crate::parsing::ParsingState::Error(msg),
-                    crate::parsing::ParsingState::EOF => {
-                        return crate::parsing::ParsingState::Error(format!(
-                            "Parsing failed ({})",
-                            lexer.get_current_position()
-                        ))
-                    }
-                }
-
-                let delay_state = <crate::types::Duration as crate::types::Token<crate::types::Duration>>::parse(lexer);
-                match delay_state {
-                    crate::parsing::ParsingState::Finished(value) => delay = value,
-                    crate::parsing::ParsingState::Error(msg) => return crate::parsing::ParsingState::Error(msg),
-                    crate::parsing::ParsingState::EOF => {
-                        return crate::parsing::ParsingState::Error(format!(
-                            "Parsing failed ({})",
-                            lexer.get_current_position()
-                        ))
-                    }
-                }
-
-                crate::parsing::ParsingState::Finished($manager_name::new(rate, delay))
-            }
-        }
-    }
-}
-
-/// With priority
-
-/// A macro to conditionally implement `enqueue` and `dequeue` methods.
-///
-/// This macro takes a boolean value and implements the methods based on that value:
-/// - When `true`, no methods are implemented.
-/// - When `false`, the following methods are implemented:
-///   - `enqueue`: Adds a bundle to the queue.
-///   - `dequeue`: Removes a bundle from the queue.
-#[macro_export]
-macro_rules! impl_manual_queue_methods_prio {
-    (true) => {
-        // No enqueue or dequeue methods when $auto_update is false
-    };
-
-    (false) => {
-        pub fn enqueue(&mut self, bundle: &crate::bundle::Bundle) {
-            if self.queue_size[bundle.priority as usize] + bundle.size > self.original_volume {
-                panic!("Queue will overflow the contact's volume");
-            }
-            for prio in 0..bundle.priority as usize + 1 {
-                self.queue_size[prio] += bundle.size;
-            }
-        }
-
-        pub fn dequeue(&mut self, bundle: &crate::bundle::Bundle) {
-            if self.queue_size[bundle.priority as usize] < bundle.size {
-                panic!("Attempting to dequeue a bundle larger than the current queue size");
-            }
-
-            for prio in 0..bundle.priority as usize + 1 {
-                self.queue_size[prio] += bundle.size;
+            #[inline(always)]
+            fn dequeue(&mut self, bundle: &crate::bundle::Bundle)  {
+                self.queue_size -= bundle.size;
             }
         }
     };
-}
 
-#[macro_export]
-macro_rules! generate_prio_volume_manager {
-    ($manager_name:ident, $add_delay:tt, $auto_update:tt, $prio_count:tt)  => {
-        /// A simple manager for handling volcontact_data.startume and/or transmission delays (macro generated).
+    // Safe guard $prio_count, a value of 0 would still compile..
+    ($manager_name:ident, 0) => {
+         compile_error!("To disable priority, set $prio_count to 1");
+    };
+
+     // if the priority count is different than one, queue_size is an array
+    ($manager_name:ident, $prio_count:tt) => {
+
         #[cfg_attr(feature = "debug", derive(Debug))]
         pub struct $manager_name {
             /// The data transmission rate.
@@ -300,14 +87,40 @@ macro_rules! generate_prio_volume_manager {
                 Self {
                     rate,
                     delay,
-                    queue_size: [0.0; 3],
+                    queue_size: [0.0; $prio_count],
                     original_volume: 0.0,
                 }
             }
 
-            crate::impl_manual_queue_methods_prio!($auto_update);
+            #[inline(always)]
+            fn get_queue_size(&self, bundle: &crate::bundle::Bundle) -> crate::types::Volume {
+                    self.queue_size[bundle.priority as usize]
+            }
+            #[inline(always)]
+            fn enqueue(&mut self, bundle: &crate::bundle::Bundle)  {
+                for prio in 0..bundle.priority as usize + 1 {
+                    self.queue_size[prio] += bundle.size;
+                }
+            }
+            #[inline(always)]
+            fn dequeue(&mut self, bundle: &crate::bundle::Bundle)  {
+                for prio in 0..bundle.priority as usize + 1 {
+                    self.queue_size[prio] -= bundle.size;
+                }
+            }
         }
-            impl crate::contact_manager::ContactManager for $manager_name {
+    };
+
+}
+
+#[macro_export]
+macro_rules! generate_prio_volume_manager {
+
+    ($manager_name:ident, $add_delay:tt, $auto_update:tt, $prio_count:tt)  => {
+
+        crate::generate_struct_management!($manager_name, $prio_count);
+
+        impl crate::contact_manager::ContactManager for $manager_name {
             /// Simulates the transmission of a bundle based on the contact data and available free intervals.
             ///
             #[doc = concat!( "The transmission time start time will be offset by the queue size: ", stringify!($add_delay),"`.")]
@@ -327,16 +140,19 @@ macro_rules! generate_prio_volume_manager {
                 at_time: crate::types::Date,
                 bundle: &crate::bundle::Bundle,
             ) -> Option<crate::contact_manager::ContactManagerTxData> {
-                let b_prio = bundle.priority as usize;
-                if bundle.size > self.original_volume - self.queue_size[b_prio] {
+
+                // This function call should be expanded at compile time
+                let queue_size = self.get_queue_size(&bundle);
+
+                if bundle.size > self.original_volume - queue_size {
+                    println!("{}", queue_size);
                     return None;
                 }
 
-
                 let mut contact_start = contact_data.start;
                 // add_delay case 1 : if not eto, we push the eto from the contact start time
-                if ($auto_update && $add_delay) {
-                    contact_start += self.queue_size[b_prio] / self.rate;
+                if ($add_delay && $auto_update) {
+                    contact_start += queue_size / self.rate;
                 }
                 let mut tx_start = if (contact_start > at_time) {
                     contact_start
@@ -345,8 +161,8 @@ macro_rules! generate_prio_volume_manager {
                 };
 
                 // add_delay case 2 : eto, bundles are still in queue
-                if (!$auto_update && $add_delay) {
-                    tx_start += self.queue_size[b_prio] / self.rate;
+                if ($add_delay && !$auto_update) {
+                    tx_start += queue_size / self.rate;
                 }
 
                 let tx_end = tx_start + bundle.size / self.rate;
@@ -386,9 +202,7 @@ macro_rules! generate_prio_volume_manager {
                     // Conditionally update queue size based on $auto_update
                     // Can overflow with overbooking
                     if $auto_update {
-                        for prio in 0..bundle.priority as usize + 1 {
-                            self.queue_size[prio] += bundle.size;
-                        }
+                        self.enqueue(bundle);
                     }
                     return Some(data);
                 }
@@ -406,9 +220,6 @@ macro_rules! generate_prio_volume_manager {
             /// Returns `true` if initialization is successful, or `false` if there are gaps in the intervals.
             fn try_init(&mut self, contact_data: &crate::contact::ContactInfo) -> bool {
                 self.original_volume = (contact_data.end - contact_data.start) * self.rate;
-                for i in 0..self.queue_size.len() {
-                    self.queue_size[i] = self.original_volume;
-                }
                 true
             }
 
